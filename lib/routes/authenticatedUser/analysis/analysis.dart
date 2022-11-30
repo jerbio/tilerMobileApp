0d90c67e477963693d83fc338c7ab86c1e6a61ac
jeromebiotidara <jeromebiotidara@gmail.com>
2022-11-30 01:06:07 +0000

    created routes for analysis ui

---
+++ lib/routes/authenticatedUser/analysis/analysis.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Analysis extends StatefulWidget {
+  @override
+  AnalysisState createState() => AnalysisState();
+}
+
+class AnalysisState extends State<Analysis> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/routes/authenticatedUser/analysis/taskCategorization.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class TaskCategorization extends StatefulWidget {
+  @override
+  TaskCategorizationState createState() => TaskCategorizationState();
+}
+
+class TaskCategorizationState extends State<TaskCategorization> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('we are categorized'),
+    );
+  }
+}
