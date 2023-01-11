b63124bd88287f6d49a7ec3e38b20b0509c83a78
JMac Jerbio <jeromebiotidara@gmail.com>
2023-01-10 21:43:20 -0700

    Added more png and building autoentry

---
+++ lib/data/adHoc/autoData.dart
@@ -0,0 +1,151 @@
+import 'package:tiler_app/util.dart';
+
+var images = [
+  {
+    'durations': [Utility.oneMin],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_we_are_not_Gods_064eb660-6cca-44bb-b75b-12da4555ef30.png',
+  },
+  {
+    'durations': [Utility.thirtyMin, Utility.oneHour],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_build_a_pillow_fort_d77bbb89-2e18-4e70-a547-a78d9b9eee83.png'
+  },
+  {
+    'durations': [
+      Utility.thirtyMin,
+      Utility.oneHour,
+      Duration(hours: 2),
+      Duration(hours: 4),
+      Duration(hours: 6),
+      Duration(hours: 24)
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_cute_photo_of_woman_and_man_working_together_looking__49c9615b-3c01-4914-aa4c-be6aaeba779d.png'
+  },
+  {
+    'durations': [
+      Utility.thirtyMin,
+      Utility.oneHour,
+      Duration(hours: 2),
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_Do_some_origami_43731a0a-393c-434e-ab3e-1c5bb10143b2.png'
+  },
+  {
+    'durations': [
+      Utility.fifteenMin,
+      Utility.thirtyMin,
+      Utility.oneHour,
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_feeling_idle_excited_at_a_dance_party_2efb40a6-7b44-49d9-a038-50f197bb506a.png'
+  },
+  {
+    'descriptions': ['Watch a movie marathon', 'Binge Watch'],
+    'durations': [
+      Duration(hours: 2),
+      Duration(hours: 4),
+      Duration(hours: 6),
+      Duration(hours: 24)
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_Have_a_movie_marathon_e574a17c-3c4a-439b-8c94-7ae1771495f4.png'
+  },
+  {
+    'descriptions': [
+      'Work on a project',
+      'Work at a shelter',
+      'Join a Maker shop'
+    ],
+    'durations': [
+      Utility.oneHour,
+      Duration(hours: 2),
+      Duration(hours: 4),
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_image_of_someone_creating_or_making_something_or_a_ma_2a50defa-b851-4671-9cfd-41eaa5636130.png'
+  },
+  {
+    'descriptions': ['Make a bird feeder', 'Work at a shelter'],
+    'durations': [
+      Utility.thirtyMin,
+      Utility.oneHour,
+      Duration(hours: 2),
+      Duration(hours: 4)
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_Make_a_bird_feeder_28f817f4-f638-4730-bf9c-4273f6a6ef33.png'
+  },
+  {
+    'descriptions': ['Reorganize Closet', 'Reorganize room'],
+    'durations': [
+      Duration(hours: 2),
+      Duration(hours: 4),
+      Duration(hours: 6),
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_Reorganize_your_closets_abdd967a-9592-444b-b772-0326fa46d8ea.png'
+  },
+  {
+    'descriptions': ['Plant indoor Garden', 'Pick up planting'],
+    'durations': [
+      Duration(hours: 2),
+      Duration(hours: 4),
+      Duration(hours: 6),
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_plant_indoor_garden_fb195907-8fd8-4c03-b998-61eb0d000f5f'
+  },
+  {
+    'descriptions': ['Make a T-shirt'],
+    'durations': [
+      Duration(hours: 2),
+      Duration(hours: 4),
+      Duration(hours: 6),
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_plant_indoor_garden_fb195907-8fd8-4c03-b998-61eb0d000f5f.pmg'
+  },
+  {
+    'descriptions': ['Take a nap'],
+    'durations': [
+      Duration(hours: 1),
+      Duration(hours: 2),
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_take_a_nap_d7b1437a-eb98-458b-ad54-50ccd9792ea1.png'
+  },
+  {
+    'descriptions': ['Go for a walk'],
+    'durations': [
+      Duration(minutes: 30),
+      Duration(hours: 1),
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_go_for_a_walk_at_different_time_of_the_day_and_differ_e708108b-c576-4e27-a01c-dce2a88c7599.png'
+  },
+  {
+    'descriptions': ['Listen to music', 'Listen to your favorite artist'],
+    'durations': [
+      Duration(minutes: 5),
+      Duration(minutes: 15),
+      Duration(minutes: 30),
+      Duration(hours: 1),
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_people_trying_to_find_something_to_do_while_listening_7fcfd84e-fde9-44b1-8835-470d9ff7036d.png'
+  },
+  {
+    'descriptions': ['Redesign room', 'Rearrange home'],
+    'durations': [
+      Duration(minutes: 30),
+      Duration(hours: 1),
+      Duration(hours: 2),
+      Duration(hours: 4),
+      Duration(hours: 12),
+    ],
+    'asset':
+        'assets/images/regenerativeArt/pictimely_decorate_a_room_05c28f1e-30fb-44f9-9738-30fe5decab93.png'
+  }
+];
