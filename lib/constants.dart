5d1a93c478ef11e346de959dfff584c6b5e00405
jerbio <jeromebiotidara@gmail.com>
2023-02-16 19:11:32 -0700

    Build number upgrade

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,9 @@
-const bool isProduction = true;
+const bool isProduction = false;
 const bool isDebug = !isProduction;
 const bool isRemote = true;
 const prodDomain = 'localhost-44322-tiler-prod.conveyor.cloud';
 const String devDomain =
-    isRemote ? 'localhost-44388-x-if7.conveyor.cloud' : '192.168.1.4:45457';
+    isRemote ? 'localhost-44388-x-if7.conveyor.cloud' : '192.168.1.4:45458';
 // const String devDomain = isDebug ? '10.0.2.2:44322' : 'www.tiler.app';
 const String tilerDomain = isProduction ? prodDomain : devDomain;
 const int stateRetrievalRetry = 100;
