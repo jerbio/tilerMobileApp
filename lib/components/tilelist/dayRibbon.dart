3db0181b84993796118d0346c8dfe3eca3b28c44
jerbio <jeromebiotidara@gmail.com>
2023-04-22 05:01:22 -0600

    Created lib/components/tilelist/dayRibbon.dart files

---
+++ lib/components/tilelist/dayRibbon.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DayRibbon extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _DayRibbonState();
+}
+
+class _DayRibbonState extends State<DayRibbon> {
+  @override
+  Widget build(BuildContext context) {}
+}
