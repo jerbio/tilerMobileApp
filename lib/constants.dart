ba164fb0d00d17c069fb6b6944f2f8c350c1f4cf
jerbio <jeromebiotidara@gmail.com>
2023-06-05 15:59:34 -0600

    Implemented partial functioning of mobile external confirmation

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,12 @@
-const bool isProduction = true;
+const bool isProduction = false;
 const bool isDebug = !isProduction;
 const bool isRemote = true;
 const prodDomain = 'localhost-44322-tiler-prod.conveyor.cloud';
-const String devDomain = isRemote
-    ? 'localhost-44388-x-if7.conveyor.cloud'
-    : 'tilerfront.conveyor.cloud';
-// const String devDomain = isDebug ? '10.0.2.2:44322' : 'www.tiler.app';
+// const String devDomain = isRemote
+//     ? 'localhost-44388-x-if7.conveyor.cloud'
+//     : 'tilerfront.conveyor.cloud';
+const String devDomain =
+    isDebug ? '10.0.2.2:44388' : 'tilerfront.conveyor.cloud';
 const String tilerDomain = isProduction ? prodDomain : devDomain;
 const int stateRetrievalRetry = 100;
 const int onTextChangeDelayInMs = 500;
