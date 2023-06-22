9dc83430d709e23c068a9f9bec15f4887d562b4d
jerbio <jeromebiotidara@gmail.com>
2023-06-22 00:27:03 -0600

    Implemented a sign in by google still need to implement auto close from the server side

--- lib/services/api/authorization.dart
+++ lib/services/api/authorization.dart
@@ -1,14 +1,20 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:tiler_app/data/request/TilerError.dart';
 import 'package:tiler_app/services/api/authenticationData.dart';
 import 'package:tiler_app/services/api/googleSignInApi.dart';
 import 'package:tiler_app/services/api/thirdPartyAuthenticationData.dart';
 import 'package:tiler_app/services/api/userPasswordAuthenticationData.dart';
+import 'package:tiler_app/services/localAuthentication.dart';
 import '../../constants.dart' as Constants;
 import 'package:tiler_app/services/api/appApi.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'package:url_launcher/url_launcher.dart';
 
 class AuthorizationApi extends AppApi {
   Future<UserPasswordAuthenticationData> registerUser(
@@ -60,12 +66,157 @@ class AuthorizationApi extends AppApi {
     }
   }
 
+  Future<ThirdPartyAuthenticationData?> iosSignIn() async {
+    final List<String> requestedScopes = Constants.googleApiScopes;
+    String clientId = dotenv.env[Constants.googleClientDefaultKey]!;
+    String nonParsedURI =
+        'https://${Constants.tilerDomain}/account/googleMobileSignIn';
+    String redirectUri = Uri.encodeComponent(nonParsedURI);
+
+    Future<String> getAuthorizationUrl() async {
+      final authUrl = Uri.parse('https://accounts.google.com/o/oauth2/auth?'
+          'client_id=$clientId&'
+          'redirect_uri=$redirectUri&'
+          'response_type=code&'
+          'scope=${requestedScopes.join(' ')}');
+
+      return authUrl.toString();
+    }
+
+    Future<ThirdPartyAuthenticationData?> handleCallbackAndExchangeToken(
+        Uri uri) async {
+      if (uri.queryParameters.containsKey('refreshToken') &&
+          uri.queryParameters['refreshToken'] != null) {
+        final queryParameter = uri.queryParameters;
+        String? accessToken = queryParameter['accessToken']!;
+        String? refreshToken = queryParameter['refreshToken']!;
+        String? providerId = queryParameter['providerId']!;
+        String? email = queryParameter['email']!;
+
+        ThirdPartyAuthenticationData retValue =
+            await ThirdPartyAuthenticationData.getThirdPartyuthentication(
+                accessToken, refreshToken, email, 'google', providerId);
+
+        if (!retValue.isDefault) {
+          Authentication localAuthentication = new Authentication();
+          await localAuthentication.saveCredentials(retValue);
+        }
+
+        return retValue;
+      }
+      return null;
+    }
+
+    MethodChannel _channel = MethodChannel(
+        'TILER_THIRD_PARTY_SIGNUP_CHANNEL'); // Replace with the name of your Flutter method channel
+
+// Listen for the authorization callback and handle it
+    _channel.setMethodCallHandler((call) async {
+      print("THE CHANNEL IS TALKING ");
+      print("call.method -> ${call.method} ");
+      if (call.method == 'handleAuthorizationCallback') {
+        final uri = Uri.parse(call.arguments as String);
+        print("uri parsed -> ${uri} ");
+        print("uri queryParameters -> ${uri.queryParameters} ");
+
+        AuthenticationData? authenticationData =
+            await handleCallbackAndExchangeToken(uri);
+      }
+    });
+    try {
+      final authorizationUrl = await getAuthorizationUrl();
+      if (await canLaunch(authorizationUrl)) {
+        Uri authorizationUri = Uri.parse(authorizationUrl);
+        await launchUrl(authorizationUri);
+      }
+    } catch (error) {
+      print(error);
+      // Handle sign-in error
+    }
+    ThirdPartyAuthenticationData? retValue;
+    Function retryLogin = () async {
+      Authentication localAuthentication = new Authentication();
+      var thirdParty = await localAuthentication.readCredentials();
+      if (thirdParty != null &&
+          thirdParty is ThirdPartyAuthenticationData &&
+          !thirdParty.isDefault) {
+        retValue = thirdParty;
+      }
+    };
+    int counter = Constants.retryLoginCount;
+    while (retValue == null && counter > 0) {
+      var futureUpdate = Future.delayed(Constants.retryLoginDuration, () async {
+        await retryLogin();
+        counter--;
+      });
+      await futureUpdate;
+    }
+    if (counter <= 0 && retValue == null) {
+      throw TilerError(message: "Google login timed out");
+    }
+
+    return retValue;
+  }
+
   Future<AuthenticationData?> signInToGoogle() async {
+    if (Platform.isIOS) {
+      return await iosSignIn();
+    }
+    return await processAndroidGoogleLogin();
+  }
+
+  Future<ThirdPartyAuthenticationData> getBearerToken(
+      {required String email,
+      required String accessToken,
+      required String refreshToken,
+      required String displayName,
+      required String providerId,
+      required String thirdpartyType}) async {
+    String tilerDomain = Constants.tilerDomain;
+    String url = tilerDomain;
+    Uri uri = Uri.https(url, 'account/MobileExternalLogin');
+    String providerName = 'Google';
+    Map<String, dynamic> parameters = {
+      'Email': email,
+      'AccessToken': accessToken,
+      'DisplayName': displayName,
+      'ProviderKey': providerId,
+      'ThirdPartyType': thirdpartyType,
+      'RefreshToken': refreshToken
+    };
+
+    var response = await http.post(uri,
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: parameters,
+        encoding: Encoding.getByName("utf-8"));
+
+    if (response.statusCode == 200) {
+      var jsonResult = jsonDecode(response.body);
+      if (isJsonResponseOk(jsonResult)) {
+        ThirdPartyAuthenticationData retValue =
+            await ThirdPartyAuthenticationData.getThirdPartyuthentication(
+                accessToken, refreshToken, email, providerName, providerId);
+        return retValue;
+      }
+      String tilerErrorMessage = errorMessage(jsonResult);
+      TilerError tilerError = TilerError(message: tilerErrorMessage);
+      throw tilerError;
+    }
+
+    TilerError tilerError = TilerError(message: 'Failed to authenticate user');
+    throw tilerError;
+  }
+
+  Future<AuthenticationData?> processAndroidGoogleLogin() async {
+    String clientId = dotenv.env[Constants.googleClientIdKey]!;
+    String clientSecret = dotenv.env[Constants.googleClientSecretKey]!;
+    final requestedScopes = Constants.googleApiScopes;
     Future<Map<String, dynamic>> getRefreshToken(String clientId,
         String clientSecret, String serverAuthCode, List<String> scopes) async {
       final String refreshTokenEndpoint = 'https://oauth2.googleapis.com/token';
 
       final Map<String, dynamic> requestBody = {
+        'access_type': 'offline',
         'client_id': clientId,
         'client_secret': clientSecret,
         'grant_type': 'authorization_code',
@@ -73,6 +224,8 @@ class AuthorizationApi extends AppApi {
         'redirect_uri': 'https://${Constants.tilerDomain}/signin-google',
         'scope': scopes.join(' '),
       };
+
+      print(requestBody);
 
       final http.Response response = await http.post(
         Uri.parse(refreshTokenEndpoint),
@@ -83,8 +236,6 @@ class AuthorizationApi extends AppApi {
       if (response.statusCode == 200) {
         final Map<String, dynamic> responseData = jsonDecode(response.body);
         return responseData;
-        // final String refreshToken = responseData['refresh_token'];
-        // return refreshToken;
       } else {
         throw Exception('Failed to authenticate user account');
       }
@@ -96,22 +247,14 @@ class AuthorizationApi extends AppApi {
       var authHeaders = await googleUser.authHeaders;
       print(authHeaders);
 
-      String clientId = dotenv.env[Constants.googleClientIdKey]!;
-      String clientSecret = dotenv.env[Constants.googleClientSecretKey]!;
-
       String? refreshToken;
       String? accessToken = googleAuthentication.accessToken;
-      if (googleUser.serverAuthCode != null) {
+      String? serverAuthCode =
+          googleUser.serverAuthCode ?? googleAuthentication.idToken;
+      if (serverAuthCode != null) {
         refreshToken = googleAuthentication.idToken!;
-        final List<String> requestedScopes = [
-          'https://www.googleapis.com/auth/calendar',
-          'https://www.googleapis.com/auth/calendar.events.readonly',
-          "https://www.googleapis.com/auth/calendar.readonly",
-          "https://www.googleapis.com/auth/calendar.events",
-          'https://www.googleapis.com/auth/userinfo.email'
-        ];
-        Map serverResponse = await getRefreshToken(clientId, clientSecret,
-            googleUser.serverAuthCode!, requestedScopes);
+        Map serverResponse = await getRefreshToken(
+            clientId, clientSecret, serverAuthCode, requestedScopes);
 
         refreshToken = serverResponse['refresh_token'];
         accessToken = serverResponse['access_token'];
@@ -128,28 +271,13 @@ class AuthorizationApi extends AppApi {
         'ThirdPartyType': providerName,
         'RefreshToken': refreshToken
       };
-
-      var response = await http.post(uri,
-          headers: {"Content-Type": "application/x-www-form-urlencoded"},
-          body: parameters,
-          encoding: Encoding.getByName("utf-8"));
-
-      if (response.statusCode == 200) {
-        var jsonResult = jsonDecode(response.body);
-        if (isJsonResponseOk(jsonResult)) {
-          ThirdPartyAuthenticationData retValue =
-              await ThirdPartyAuthenticationData.getThirdPartyuthentication(
-                  accessToken!,
-                  refreshToken!,
-                  googleUser.email,
-                  providerName,
-                  googleUser.id);
-          return retValue;
-        }
-        String tilerErrorMessage = errorMessage(jsonResult);
-        TilerError tilerError = TilerError(message: tilerErrorMessage);
-        throw tilerError;
-      }
+      return await getBearerToken(
+          accessToken: accessToken!,
+          email: googleUser.email,
+          providerId: googleUser.id,
+          refreshToken: refreshToken!,
+          displayName: googleUser.displayName!,
+          thirdpartyType: providerName);
     }
     throw TilerError();
   }
