75eb72d3972de56aa73e1266ab993f5a5a47c33d
jerbio <jeromebiotidara@gmail.com>
2024-01-13 01:13:25 -0700

    Implemented better UI alignments for defer all text, Also shrinked margins between tiles

--- lib/components/tileUI/tileAddress.dart
+++ lib/components/tileUI/tileAddress.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:tiler_app/data/subCalendarEvent.dart';
 import 'package:maps_launcher/maps_launcher.dart';
 import 'package:tiler_app/styles.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TileAddress extends StatefulWidget {
   SubCalendarEvent subEvent;
@@ -14,8 +15,15 @@ class TileAddress extends StatefulWidget {
 }
 
 class TileAddressState extends State<TileAddress> {
+  Future<void> _launchUrl(Uri url) async {
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    bool isAddressTexturl = false;
     String? addressString = widget.subEvent.searchdDescription != null
         ? widget.subEvent.searchdDescription
         : widget.subEvent.address;
@@ -23,9 +31,13 @@ class TileAddressState extends State<TileAddress> {
         ? widget.subEvent.addressDescription
         : addressString;
     if (addressString != null && addressString.isNotEmpty) {
-      addressString =
-          addressString[0].toUpperCase() + addressString.substring(1);
+      isAddressTexturl = Uri.parse(addressString).isAbsolute;
+      if (!isAddressTexturl) {
+        addressString =
+            addressString[0].toUpperCase() + addressString.substring(1);
+      }
     }
+
     return GestureDetector(
       onTap: () async {
         String? addressLookup = this.widget.subEvent.address;
@@ -36,6 +48,11 @@ class TileAddressState extends State<TileAddress> {
           addressLookup = this.widget.subEvent.searchdDescription;
         }
         if (addressLookup != null) {
+          if (isAddressTexturl) {
+            final Uri url = Uri.parse(addressLookup);
+            await _launchUrl(url);
+            return;
+          }
           MapsLauncher.launchQuery(addressLookup);
         }
       },
@@ -49,7 +66,9 @@ class TileAddressState extends State<TileAddress> {
               decoration: TileStyles.tileIconContainerBoxDecoration,
               margin: const EdgeInsets.fromLTRB(0, 0, 20, 0),
               child: Icon(
-                Icons.location_on_rounded,
+                !isAddressTexturl
+                    ? Icons.location_on_rounded
+                    : Icons.link_outlined,
                 color: TileStyles.defaultTextColor,
                 size: TileStyles.tileIconSize,
               ),
