8a9dcd609bcfe7d25332f5c561cefc56ae5fbb23
jerbio <jeromebiotidara@gmail.com>
2024-03-29 19:30:55 -0600

    Implemented account deletion and hide sign in with google on sign up

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,14 +1,13 @@
 import 'dart:io';
 
-const bool isProduction = true;
+const bool isProduction = false;
 const bool isDebug = !isProduction;
-const bool isRemote = true;
+const bool isRemote = false;
 // const prodDomain = 'localhost-44322-tiler-prod.conveyor.cloud';
 const prodDomain = 'localhost-44322-tiler-prod.conveyor.cloud';
 // const String devDomain = 'localhost-44388-x-if7.conveyor.cloud';
-const String devDomain = isRemote
-    ? 'localhost-44388-x-if7.conveyor.cloud'
-    : 'tilerfront.conveyor.cloud';
+const String devDomain =
+    isRemote ? 'localhost-44388-x-if7.conveyor.cloud' : '10.0.2.2:44322';
 const String tilerDomain = isProduction ? prodDomain : devDomain;
 const int stateRetrievalRetry = 100;
 const int onTextChangeDelayInMs = 700;
