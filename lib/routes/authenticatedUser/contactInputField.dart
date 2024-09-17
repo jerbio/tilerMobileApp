f0b2510169a8a6590f4f3a3cc2eb1d6c967ff448
jerbio <jeromebiotidara@gmail.com>
2024-09-17 00:52:07 -0600

    Implemented contact field button. Still need to create tileshare page and use contact fields as expected

---
+++ lib/routes/authenticatedUser/contactInputField.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:tiler_app/data/contact.dart';
+
+class ContactInputField extends StatefulWidget {
+  final Function? onContactUpdate;
+  ContactInputField({this.onContactUpdate});
+  @override
+  _ContactInputFieldState createState() => _ContactInputFieldState();
+}
+
+class _ContactInputFieldState extends State<ContactInputField> {
+  final emailRegex = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+  final phoneRegex = RegExp(r"^\+?[0-9]{7,15}$");
+  final TextEditingController _controller = TextEditingController();
+  final List<Contact> _contacts = [];
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  bool _isValidContact(String input) {
+    // Simple email and phone number validation
+
+    return emailRegex.hasMatch(input) || phoneRegex.hasMatch(input);
+  }
+
+  void _addContact(String contactVal) {
+    if (_isValidContact(contactVal)) {
+      setState(() {
+        final contactObj = Contact();
+        if (emailRegex.hasMatch(contactVal)) {
+          contactObj.emailNumber = contactVal;
+        }
+        if (phoneRegex.hasMatch(contactVal)) {
+          contactObj.phoneNumber = contactVal;
+        }
+        _contacts.add(contactObj);
+      });
+      _controller
+          .clear(); // Clear the text input field after adding the contact
+    }
+    if (this.widget.onContactUpdate != null) {
+      this.widget.onContactUpdate!(_contacts);
+    }
+  }
+
+  void _removeContact(Contact contact) {
+    setState(() {
+      _contacts.remove(contact);
+    });
+    if (this.widget.onContactUpdate != null) {
+      this.widget.onContactUpdate!(_contacts);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 8.0,
+            children: [
+              _buildTextField(),
+              ..._contacts.map((contact) => _buildPill(contact)).toList(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextField() {
+    return Container(
+      width: 150, // You can adjust the width or make it responsive
+      child: TextField(
+        controller: _controller,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: "Add contact",
+        ),
+        onSubmitted: (value) {
+          _addContact(value);
+        },
+        onEditingComplete: () {
+          _addContact(_controller.text);
+        },
+      ),
+    );
+  }
+
+  Widget _buildPill(Contact contact) {
+    return Chip(
+      label: Text(contact.emailNumber ?? contact.phoneNumber ?? ""),
+      deleteIcon: Icon(Icons.close),
+      onDeleted: () => _removeContact(contact),
+      backgroundColor: Colors.blueAccent.shade100,
+      labelStyle: TextStyle(color: Colors.white),
+    );
+  }
+}
