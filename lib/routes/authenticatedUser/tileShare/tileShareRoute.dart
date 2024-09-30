022a6b1292397c0bf603065f54e37d5b22481dbb
jerbio <jeromebiotidara@gmail.com>
2024-09-29 23:27:06 -0600

    Cleaned up tile share ui app bar info

--- lib/routes/authenticatedUser/tileShare/tileShareRoute.dart
+++ lib/routes/authenticatedUser/tileShare/tileShareRoute.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:tiler_app/routes/authenticatedUser/tileShare/designatedTileListWidget.dart';
 import 'package:tiler_app/routes/authenticatedUser/tileShare/tileClusterWidget.dart';
+import 'package:tiler_app/styles.dart';
 
 class TileShareRoute extends StatefulWidget {
   @override
@@ -15,14 +16,40 @@ class _TileShareState extends State<TileShareRoute> {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
+          // automaticallyImplyLeading: false,
+          centerTitle: true,
+          backgroundColor: TileStyles.appBarColor,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(
+                Icons.share,
+                color: TileStyles.appBarTextColor,
+              ),
+              SizedBox.square(
+                dimension: 5,
+              ),
+              Text(
+                AppLocalizations.of(context)!.tileShare,
+                style: TileStyles.titleBarStyle,
+              )
+            ],
+          ),
           bottom: const TabBar(
             tabs: [
-              Tab(icon: Icon(Icons.add)),
-              Tab(icon: Icon(Icons.list)),
+              Tab(
+                icon: Icon(
+                  Icons.add,
+                  color: TileStyles.primaryContrastColor,
+                ),
+              ),
+              Tab(
+                  icon:
+                      Icon(Icons.list, color: TileStyles.primaryContrastColor)),
               // Tab(icon: Icon(Icons.directions_bike)),
             ],
           ),
-          title: Text(AppLocalizations.of(context)!.tileShare),
+          // title: Text(AppLocalizations.of(context)!.tileShare),
         ),
         body: TabBarView(
           children: [
