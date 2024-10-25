dff3026f64c23a5fdb956f2695b1d9c4d9a99fce
eyadfhafi <eyadhafi13@gmail.com>
2024-10-25 03:28:50 +0300

    Fix conflicts and adjustments after merge

--- lib/routes/authenticatedUser/contactInputField.dart
+++ lib/routes/authenticatedUser/contactInputField.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:tiler_app/components/tilelist/tileBatch.dart';
+import 'package:tiler_app/components/tilelist/dailyView/tileBatch.dart';
 import 'package:tiler_app/data/contact.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:tiler_app/styles.dart';
