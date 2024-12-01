32b811572a4fe07bd6da7757a6f4bbedee10841b
jerbio <jeromebiotidara@gmail.com>
2024-12-01 01:12:55 -0700

    Need to update the forecast functionality

--- lib/data/ForecastResponse.dart
+++ lib/data/ForecastResponse.dart
@@ -1,4 +1,6 @@
 import 'package:tiler_app/data/calendarEvent.dart';
+import 'package:tiler_app/data/travelTimeAndDistance.dart';
+import 'package:tiler_app/util.dart';
 
 class ForecastResponse {
   int? deadlineSuggestion;
@@ -8,6 +10,7 @@ class ForecastResponse {
   int? conflictingCount;
   bool? isViable;
   List<PeekDay>? peekDays;
+  List<TravelTimeAndDistance>? travelTimeAndDistances;
 
   ForecastResponse({
     this.deadlineSuggestion,
@@ -19,23 +22,43 @@ class ForecastResponse {
     this.peekDays,
   });
 
-// fromJson factory constructor
-  factory ForecastResponse.fromJson(Map<String, dynamic> json) {
-    return ForecastResponse(
-      deadlineSuggestion: json['deadlineSuggestion'] as int?,
-      sleepDeadlineSuggestion: json['sleepDeadlineSuggestion'] as int?,
-      riskCalendarEvents: (json['riskCalendarEvents'] as List<dynamic>?)
-          ?.map((e) => CalendarEvent.fromJson(e as Map<String, dynamic>))
-          .toList(),
-      conflicts: (json['conflicts'] as List<dynamic>?)
-          ?.map((e) => CalendarEvent.fromJson(e as Map<String, dynamic>))
-          .toList(),
-      conflictingCount: json['conflictingCount'] as int?,
-      isViable: json['isViable'] as bool?,
-      peekDays: (json['peekDays'] as List<dynamic>?)
-          ?.map((e) => PeekDay.fromJson(e as Map<String, dynamic>))
-          .toList(),
-    );
+  ForecastResponse.fromJson(Map<String, dynamic> json) {
+    deadlineSuggestion = json['deadlineSuggestion'] as int?;
+    sleepDeadlineSuggestion = json['sleepDeadlineSuggestion'] as int?;
+    riskCalendarEvents = (json['riskCalendarEvents'] as List<dynamic>?)
+        ?.map((e) => CalendarEvent.fromJson(e as Map<String, dynamic>))
+        .toList();
+    conflicts = (json['conflicts'] as List<dynamic>?)
+        ?.map((e) => CalendarEvent.fromJson(e as Map<String, dynamic>))
+        .toList();
+    conflictingCount = json['conflictingCount'] as int?;
+    isViable = json['isViable'] as bool?;
+    peekDays = (json['peekDays'] as List<dynamic>?)
+        ?.map((e) => PeekDay.fromJson(e as Map<String, dynamic>))
+        .toList();
+    if (json.containsKey('travel') && json['travel'] != null) {
+      Map<String, dynamic>? travelTimeAndDistanceJson =
+          json['travel'] as Map<String, dynamic>?;
+      if (travelTimeAndDistanceJson != null) {
+        Map<int, TravelTimeAndDistance> dayIndexToTravelTimeAndDistance = {};
+        travelTimeAndDistanceJson.entries.forEach((element) {
+          String? dayEndString = element.key;
+          int? dayEndInMs = int.tryParse(dayEndString);
+
+          if (dayEndInMs != null) {
+            DateTime dayEndTime =
+                DateTime.fromMillisecondsSinceEpoch(dayEndInMs);
+            TravelTimeAndDistance travelTimeAndDistance =
+                TravelTimeAndDistance.fromJson(travelTimeAndDistanceJson);
+            travelTimeAndDistance.dayEndTime = dayEndTime;
+            dayIndexToTravelTimeAndDistance[Utility.getDayIndex(dayEndTime)] =
+                travelTimeAndDistance;
+          }
+        });
+        travelTimeAndDistances =
+            dayIndexToTravelTimeAndDistance.values.toList();
+      }
+    }
   }
 }
 
